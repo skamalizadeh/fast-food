b62c471dcab7136e94fc6493901f29e06730e02d
skamalizadeh <skamalizadeh@gmail.com>
2023-09-04 19:55:50 +0330

    Placeholder for images

--- src/FastFoodItem/fastFoodItem.jsx
+++ src/FastFoodItem/fastFoodItem.jsx
@@ -1,11 +1,15 @@
+import "./fastFoodItem.css";
 import { HiShoppingCart } from "react-icons/hi";
+
 const FastFoodItem = ({ name, price, ingredients, imageUrl }) => {
   return (
     <div className="card product-card h-100 border-0 shadow-sm pb-1">
       <span className="badge badge-end badge-shadow  bg-success fs-md fw-medium">
         قیمت: {price.toLocaleString()} تومان
       </span>
-      <img className="card-img-top" src={imageUrl} />
+      <div className="card__placeholder">
+        <img className="card-img-top" src={imageUrl} />
+      </div>
       <div className="card-body text-center pt-3 pb-4 d-flex flex-column">
         <h5 className="mb-2">{name}</h5>
         <div className="fs-ms fw-bold text-muted mb-3">{ingredients}</div>
